f3d40909c08304b36a8915a164ac77145ac783fe
kennethmensah <kenneth.mensah@ashesi.edu.gh>
2015-11-21 11:49:53 +0000

    Started push notifications

---
+++ app/src/main/java/com/example/streethustling/cafeteria2/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.streethustling.cafeteria2;
+
+/**
+ * Created by StreetHustling on 11/21/15.
+ */
+public class AppConfig {
+
+    public static final String PARSE_CHANNEL = "Cafeteria";
+    public static final String PARSE_APPLICATION_ID = "vO4idSeWzV9bs8X4wtbC2RLn9fwIYsffeTnav4G7";
+    public static final String PARSE_CLIENT_KEY = "lohkeQJMs30dGunfabaZw1aiZ3nyXWbRn7lz9Vt5";
+    public static final int NOTIFICATION_ID = 100;
+
+}
---
+++ app/src/main/java/com/example/streethustling/cafeteria2/ParseUtils.java
@@ -0,0 +1,8 @@
+package com.example.streethustling.cafeteria2;
+
+/**
+ * Created by StreetHustling on 11/21/15.
+ */
+public class ParseUtils {
+
+}
